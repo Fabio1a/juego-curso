494df3ee15e08af25fedea1cf4201d0da5380b11
Fabio1a <fabiola.escobedo4260@alumnos.udg.mx>
2024-07-05 01:40:12 -0600

    Delete Scripts directory

--- Scripts/Coin2D.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-signal coinCollected
-
-func _on_Coin2D_body_entered(body):
-	if body.get_name() == "jugador": 
-		body.add_Coin()
-		queue_free()
-		pass # Replace with function body.
